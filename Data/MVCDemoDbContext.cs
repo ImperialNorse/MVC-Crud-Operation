73741abda1b9d6d510b409125ff43194d91e3c2a
ImperialNorse <nimeshghimire7991@gmail.com>
2024-04-16 14:37:06 +0530

    file upload

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCrud.Models.Domain;
+
+namespace MvcCrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
